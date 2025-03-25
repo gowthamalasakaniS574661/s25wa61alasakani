a0d1f36894305003349a40ff26634776454b5e0e
gowthamalasakaniS574661 <S574661@nwmissouri.edu>
2025-03-25 15:20:33 -0500

    Modified users.js endpoint to update acc dynamically

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let ratio = 0.75;  // Define ratio
+let acc = 1000;    // Define acc
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  acc = acc * ratio;  // Update acc value
+  res.send(`Value is: ${acc}`);  // Send the updated value
 });
 
 module.exports = router;
+
